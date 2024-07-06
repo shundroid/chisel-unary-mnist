9c4317608e69168cfec01e545c1871ad618382f8
shundroid <shundroid@users.noreply.github.com>
2024-07-06 18:34:03 +0900

    feat: impl uRelu

---
+++ src/main/scala/uRelu.scala
@@ -0,0 +1,42 @@
+import chisel3._
+import chisel3.util._
+
+class uRelu(inWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val i = Input(Bool())
+    val o = Output(Bool())
+    val overhalf = Output(Bool())
+  })
+
+  val cnt = RegInit((1 << (inWidth - 1)).U(inWidth.W))
+  when (io.i & (~cnt.andR)) {
+    cnt := cnt + 1.U
+  }.elsewhen (~io.i & cnt.orR) {
+    cnt := cnt - 1.U
+  }
+
+  val overHalf = Wire(Bool())
+  overHalf := cnt(inWidth - 1)
+  io.overhalf := overHalf
+
+  val blink = RegInit(0.B)
+  blink := ~blink
+
+  io.o := Mux(overHalf, io.i, blink)
+}
+
+// for testing
+class uReluFull(inWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val i = Input(SInt(inWidth.W))
+    val o = Output(Bool())
+    val overhalf = Output(Bool())
+  })
+
+  val rng = Module(new SobolRNGDim1(inWidth))
+  rng.io.en := 1.B
+  val relu = Module(new uRelu(inWidth))
+  relu.io.i := io.i > rng.io.sobolSeq.asSInt
+  io.o := relu.io.o
+  io.overhalf := relu.io.overhalf
+}
---
+++ src/test/scala/uReluSpec.scala
@@ -0,0 +1,39 @@
+import chisel3._
+import chisel3.simulator.EphemeralSimulator._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class uReluSpec extends AnyFreeSpec with Matchers {
+  def runTestPattern(a: Int, width: Int, iter: Int = 1024): Unit = {
+    simulate(new uReluFull(width)) { dut =>
+      dut.reset.poke(true.B)
+      dut.clock.step()
+      dut.reset.poke(false.B)
+      dut.clock.step()
+
+      dut.io.i.poke(a.S)
+      dut.clock.step()
+
+      var acc = 0
+      for (i <- 0 until iter) {
+        acc += dut.io.o.peek().litValue.toInt
+        // println(s"overhalf: ${dut.io.overhalf.peek().litValue}")
+        dut.clock.step()
+      }
+
+      val output = acc / iter.toFloat * 2 - 1
+      val expected = if (a > 0) a.toFloat / (1 << (width - 1)) else 0
+      println(s"output: $output")
+      output must be (expected)
+    }
+  }
+  "uRelu should calculate 1" in {
+    runTestPattern(1, 4)
+  }
+  "uRelu should calculate -1" in {
+    runTestPattern(-1, 4)
+  }
+  "uRelu should calculate 0" in {
+    runTestPattern(0, 4)
+  }
+}
